aff3191ea182ddf37ce4d2c3d1a090c23835d2a0
jalt1 <jocelyn@ohanaparents.com>
2013-11-10 20:41:47 -0600

    Completed Phase I: Lat and Long Appearing

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,10 +7,11 @@ class AddressesController < ApplicationController
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @latitude = ?
-    # @longitude = ?
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=false"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    # The above line put the raw data into a Hash.
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
   end
 end
